338d21911df0fe304bf3aaaef32302eafa9f77bc
HammiKnuff <laws.lea@gmail.com>
2017-12-12 04:22:26 +0100

    Repaired and sorted.

--- PoliceLightBlinkenRocket/PoliceLightBlinkenRocket.ino
+++ PoliceLightBlinkenRocket/PoliceLightBlinkenRocket.ino
@@ -7,8 +7,20 @@
 void setup() {
   #define delay1 100
   #define delay2 1000
-  
 
+/*you have to setup these pins below. Even when not in use.
+if removed then rows will be turned off.
+*/
+    pinMode(0, OUTPUT);
+    pinMode(1, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+
+//Used Pins
     pinMode(8, OUTPUT);
     pinMode(9, OUTPUT);
     pinMode(10, OUTPUT);
@@ -26,38 +38,44 @@ void loop() {
   digitalWrite(14, HIGH);
   digitalWrite(13, HIGH);
   digitalWrite(12, HIGH);
+    
   delay(delay1);                      
- digitalWrite(15, LOW);
-  digitalWrite(14, LOW);
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
+      digitalWrite(15, LOW);
+      digitalWrite(14, LOW);
+      digitalWrite(13, LOW);
+      digitalWrite(12, LOW);
+        
   delay(delay1);                       
-  digitalWrite(15, HIGH);
-  digitalWrite(14, HIGH);
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
+    digitalWrite(15, HIGH);
+    digitalWrite(14, HIGH);
+    digitalWrite(13, HIGH);
+    digitalWrite(12, HIGH);
+  
   delay(delay1);                       
- digitalWrite(15, LOW);
-  digitalWrite(14, LOW);
-  digitalWrite(13, LOW);
-  digitalWrite(12, LOW);
+    digitalWrite(15, LOW);
+    digitalWrite(14, LOW);
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+  
   delay(delay1);                       
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, HIGH);
+  
+  delay(delay1);                       
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+  
+  delay(delay1);  
   digitalWrite(11, HIGH);
   digitalWrite(10, HIGH);
   digitalWrite(9, HIGH);
   digitalWrite(8, HIGH);
   delay(delay1);                       
- digitalWrite(11, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(8, LOW);
-  delay(delay1);  
-    digitalWrite(11, HIGH);
-  digitalWrite(10, HIGH);
-  digitalWrite(9, HIGH);
-  digitalWrite(8, HIGH);
-  delay(delay1);                       
- digitalWrite(11, LOW);
+  digitalWrite(11, LOW);
   digitalWrite(10, LOW);
   digitalWrite(9, LOW);
   digitalWrite(8, LOW);
